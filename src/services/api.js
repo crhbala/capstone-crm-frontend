8623bfb84eca5e5237f1999073e35d32dc48e1cc
crhbala <chrisbala8888@gmail.com>
2024-02-01 17:01:14 +0530

    api changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,1 @@
-
-
 export const LOCALHOST_URL = "https://capston-2vdh.onrender.com/api";
--- src/services/utilities/provider.js
+++ src/services/utilities/provider.js
@@ -2,14 +2,12 @@
 
 import axios from "axios";
 import { handleResponse, handleError } from "./response";
-// import { CYCLIC_BASE_URL } from "../api";
 import { LOCALHOST_URL } from "../api";
 
 // Define your api url from any source.
 // Pulling from your .env file when on the server or from localhost when locally
 const BASE_URL = LOCALHOST_URL;
 
-// const BASE_URL = CYCLIC_BASE_URL;
 
 const token = localStorage.getItem("CRM-token");
 
