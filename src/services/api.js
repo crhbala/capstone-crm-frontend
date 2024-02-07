963c2c3d00ef3c0ba7395b989bbb6d5a615c4090
crhbala <chrisbala8888@gmail.com>
2024-02-08 00:40:56 +0530

    small changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
 export const CYCLIC_BASE_URL = "https://gifted-tux-colt.cyclic.app/api";
 
-export const LOCALHOST_URL = "http://localhost:4050/api";
+export const LOCALHOST_URL = "https://capston-2vdh.onrender.com/api";
